30eed98037631ce2b9921553d2d0a3e9ae4d2de1
Jose <joseaugustowagner278@gmail.com>
2026-03-11 20:36:39 -0300

    correção

--- teste.java
+++ teste.java
@@ -2,6 +2,6 @@ public class teste {
 
     public static void main(String[] args) {
         System.out.println("hello world!");
-        System.out.printlm("opa!");
+        System.out.println("opa!");
     }
 }
